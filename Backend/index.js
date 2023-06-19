435f18e8e3735410044156f2f900489da0b5c71c
adrian <profilte82@gmail.com>
2023-06-19 11:10:58 +0100

    Las bases listas

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express();
+
+
+(async function(){
+    await mongoose.connect(process.env.URL, {
+        dbName: process.env.MONGO_DB
+    })
+console.log('Connected to DB')
+
+    app
+    .use(cors('*'))
+    .use(morgan('dev'))
+    .use(express.json())
+    .use('/api',require('./api/routes'))
+    .listen(process.env.PORT, () => {
+        console.log(`connected to port: ${process.env.PORT}`)
+    })
+})()
