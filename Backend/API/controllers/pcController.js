59b23bab571837a84a6d425852f99b640953e665
adrian <profilte82@gmail.com>
2023-10-18 11:20:42 +0100

    Rutas y controladores pc creados

--- Backend/API/controllers/pcController.js
+++ Backend/API/controllers/pcController.js
@@ -1,16 +1,58 @@
 const PcModel = require('../models/pcModel')
 
 async function newPc(req, res) {
-try {
-    const pc = await PcModel.create(req.body)
-        res.status(200).json({ name: pc.name, text: pc.text })
+    try {
+        const pc = await PcModel.create(req.body)
+        res.status(200).json({ name: pc.name, text: pc.text, price: pc.price })
 
 
-} catch (error) {
-    console.log(error)
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+async function getAllPc(req, res) {
+    try {
+        const pc = await PcModel.find()
+        res.json(pc)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function getPcById(req, res) {
+    try {
+        const pc = await PcModel.findById(req.params.id)
+        res.json(pc)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+async function updatePcById(req, res) {
+    try {
+        const pcUpdate = await PcModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        res.json(pcUpdate)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function deletePcById(req, res) {
+    try {
+        const pcDelete = await PcModel.findByIdAndDelete(req.params.id)
+        if (!pcDelete) { res.json("This computer doesn´t exist") }
+        else { res.json(pcDelete) }
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 module.exports = {
     newPc,
+    getAllPc,
+    getPcById,
+    updatePcById,
+    deletePcById
 }
--- Backend/API/routes/pcRouter.js
+++ Backend/API/routes/pcRouter.js
@@ -2,10 +2,22 @@ const router = require('express').Router()
 
 const {
     newPc,
+    getAllPc,
+    getPcById,
+    updatePcById,
+    deletePcById
+
 } = require('../controllers/pcController')
 
 router
-    .post('/newPc')
+    .post('/newPc', newPc)
+
+    .get('/', getAllPc)
+    .get('/:id', getPcById)
+
+    .put('/:id', updatePcById)
+
+    .delete('/:id', deletePcById)
 
 
 module.exports = router
